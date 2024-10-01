61044cfc0624b9eab55393b90d9f4b00ca206a8b
nishant7929 <nishantpatel7929@gmail.com>
2024-10-02 01:50:17 +0530

    Update firebase key

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,20 +1,8 @@
 const admin = require("firebase-admin");
+const firebaseCreds = require("./serviceAccountKey.json")
 
 admin.initializeApp({
-	credential: admin.credential.cert({
-		type: process.env.FIREBASE_TYPE,
-		project_id: process.env.FIREBASE_PROJECT_ID,
-		private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-		private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Ensure the private key formatting is correct
-		client_email: process.env.FIREBASE_CLIENT_EMAIL,
-		client_id: process.env.FIREBASE_CLIENT_ID,
-		auth_uri: process.env.FIREBASE_AUTH_URI,
-		token_uri: process.env.FIREBASE_TOKEN_URI,
-		auth_provider_x509_cert_url:
-			process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-		client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-		universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-	}),
+	credential: admin.credential.cert(firebaseCreds),
 	storageBucket: 'gs://andshop-4dfe2.appspot.com',
 });
 
